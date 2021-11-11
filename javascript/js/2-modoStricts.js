ad5344f3a9506b7e492e2fe9e55f77143afdbc3d
Carolina Muñoz <cm7283155@gmail.com>
2021-11-11 11:50:08 -0500

    agregando

---
+++ javascript/js/2-modoStricts.js
@@ -0,0 +1,12 @@
+"use strict"
+
+/*modo correcto*/
+
+var nombreAprendiz1 = "Raul Santi";
+console.log(nombreAprendiz1);
+
+
+/* use strict evita utilizar variables sin definir*/
+
+nombreAprendiz2 = "Joan Sebastian";
+console.log(nombreAprendiz2);
---
+++ javascript/js/4-enteros.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var costosiniva = 32000;
+var costoiva = 3200;
+var domicilio = 3000;
+var costototal = costosiniva + costoiva + domicilio;
+/*mostrar cual es el valor final del producto*/
+console.log("El costo total es de: $" + costototal);
+
+console.log("el limite para usar un entero es: " + Number.MAX_SAFE_INTEGER);
+var numMaximo = number.MAX_SAFE_INTEGER;
+/* la siguiente operacion genera un error por sobrepasar el numero maximo disponible */
+console.log(numMaximo + 1);
