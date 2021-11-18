8e896eb639cfefa4a266d4b66088ea5e86ba9fae
luisa758 <luisafw123@gmail.com>
2021-11-18 11:52:37 -0500

    modificando

--- javascript/js/12-for.js
+++ javascript/js/12-for.js
@@ -13,3 +13,17 @@ aprendiz 3:...hasta el 20
 for (i = 1; i <= 20; i++) {
     console.log("aprendiz N." + i);
 }
+var aprendiz1 = new Array();
+aprendiz1[0] = 1;
+aprendiz1[1] = "yeimi katerine";
+aprendiz1[2] = "peña ariza";
+aprendiz1[3] = 18;
+aprendiz1[4] = "B. villa del prado";
+aprendiz1[5] = 156;
+
+var aprendiz2 = new Array(2, "luisa fernana", "ramirez montoya", 19, "B.simon bolivar", 157);
+
+var aprendiz3 = [3, "jesus nicolas", "ayerbe guarnizo", 17, "B. las palmitas", 177];
+for (i = 0; i <= aprendiz1.length; i++) {
+    console.log(aprendiz1[i]);
+}
