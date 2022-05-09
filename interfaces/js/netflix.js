b088643074180349034ad195c2af304c9b0ceffb
luisa_ramirez <luisafw123@gmail.com>
2022-05-09 11:39:15 -0500

    agregando

---
+++ interfaces/js/netflix.js
@@ -0,0 +1,10 @@
+function mensajeInsidiuos() {
+    alert('¿Ya te la viste esta peli :)?')
+}
+function harry() {
+    document.getElementById("imgPrincipal").scr = "img/harry.jpeg";
+}
+function intercambiarPeliculas(ruta) {
+    document.getElementById("imgPrincipal").src = ruta;     
+
+}
